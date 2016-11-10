635dfb0489e6ef73f69bc3b109159d6d8b99663a
zachanator070 <zach.johnson070@gmail.com>
2016-11-09 23:13:37 -0700

    added video

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
-    $('select').material_select();
+
+  $('select').material_select();
 
 	$("#submitbtn").click(function() {
     	$.post('users/', $('#form').serialize());
     	return false;
-    });
-
-
   });
+
+  var songs = ["https://www.youtube.com/embed/k_5zELmun9E?autoplay=1",
+    "https://www.youtube.com/embed/a5JySIRcPFs?autoplay=1"
+  ];
+
+  var min = 0;
+  var max = songs.length -1;
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  var rand = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  $('#video').attr("src", songs[rand]);
+
+  console.log(songs[rand]);
+
+});
